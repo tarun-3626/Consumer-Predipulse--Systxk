ea92245cf7da950c0afd1e8a63313cd5ac808dff
Tarun <117096396+tarun-3626@users.noreply.github.com>
2024-11-24 09:39:40 +0530

    Create file.m

---
+++ File.m
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+sns.set()
+
+from pandas_profiling import ProfileReport
+from sklearn.preprocessing import StandardScaler
+from category_encoders.target_encoder import TargetEncoder
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_validate
+from sklearn.metrics import classification_report, roc_auc_score, accuracy_score, roc_curve, make_scorer
+
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+import warnings
+warnings.filterwarnings('ignore')
